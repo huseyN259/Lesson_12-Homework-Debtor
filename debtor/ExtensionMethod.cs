332c1ee606dc6814ecdf54709e437ce434e8890f
user <user@WINDOWS-OM6HICP>
2022-09-11 21:47:02 +0400

    Add project files.

---
+++ debtor/ExtensionMethod.cs
@@ -0,0 +1,11 @@
+﻿static class ExtensionMethod
+{
+	public static void Print<T>(this List<T> list)
+		=> list.ForEach(l => Console.WriteLine(l));
+
+	public static void Print<T>(this IEnumerable<T> collection)
+	{
+		foreach (var item in collection)
+			Console.WriteLine(item);
+	}
+}
